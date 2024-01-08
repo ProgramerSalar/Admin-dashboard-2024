69de1a3d992e63870e2849ecce6d552291b1862f
manish <manishkumar60708090@gmail.com>
2024-01-08 18:19:39 +0530

    add the lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Dashboard from "./pages/Dashboard"
+import {lazy} from 'react'
+
+
+
+
+// imports 
+const Dashboard = lazy(() => import("./pages/Dashboard"))
+
+
+
+
+
+
 
 const App = () => {
   return (
