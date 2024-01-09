362141d4600573838737158f21cec9cb15eab202
manish <manishkumar60708090@gmail.com>
2024-01-09 20:01:28 +0530

    sorting in product page

--- src/components/TableHOC.tsx
+++ src/components/TableHOC.tsx
@@ -1,12 +1,20 @@
-import { useTable, Column, TableOptions, useSortBy, usePagination } from "react-table";
-import {AiOutlineSortAscending, AiOutlineSortDescending} from "react-icons/ai"
-
+import {
+  useTable,
+  Column,
+  TableOptions,
+  useSortBy,
+  usePagination,
+} from "react-table";
+import {
+  AiOutlineSortAscending,
+  AiOutlineSortDescending,
+} from "react-icons/ai";
 
 function TableHOC<T extends Object>(
   columns: Column<T>[],
   data: T[],
-  containerClassname:string,
-  heading: string,
+  containerClassname: string,
+  heading: string
 ) {
   return function HOC() {
     const options: TableOptions<T> = {
@@ -14,52 +22,50 @@ function TableHOC<T extends Object>(
       data,
     };
 
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+      useTable(options, useSortBy);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(options,useSortBy );
+    return (
+      <div className={containerClassname}>
+        <h2 className="heading">{heading}</h2>
+        <table className="table" {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column: any) => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render("Header")}
+                    {column.isSorted && (
+                      <span>
+                        {" "}
+                        {column.isSortedDesc ? (
+                          <AiOutlineSortDescending />
+                        ) : (
+                          <AiOutlineSortAscending />
+                        )}
+                      </span>
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
 
-
-    return(
-        <div className={containerClassname}>
-            <h2 className="heading">
-                {heading}
-            </h2>
-            <table className="table" {...getTableProps()}>
-                <thead>
-                    {
-                        headerGroups.map((headerGroup) => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map((column:any) => (
-                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                    {column.render("Header")}
-                                    {
-                                        column.isSorted  && <span>{column.isSortedDesc? <AiOutlineSortDescending/> :<AiOutlineSortAscending/>}</span>
-                                    }
-                                </th>
-
-                                ))}
-                            </tr>
-                           
-                        ))
-                    }
-                </thead>
-
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row);
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => (
-                                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                                ))}
-                            </tr>
-                        )
-                    })}
-
-                </tbody>
-            </table>
-        </div>
-    )
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 }
-};
 export default TableHOC;
