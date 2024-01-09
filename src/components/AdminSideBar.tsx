27a033399ad7b4f35f0fef692fd58d0ac81c8954
manish <manishkumar60708090@gmail.com>
2024-01-09 12:33:46 +0530

    add charts and Apps

--- src/components/AdminSideBar.tsx
+++ src/components/AdminSideBar.tsx
@@ -1,28 +1,23 @@
 import { AiFillFileText } from "react-icons/ai";
 import { IoIosPeople } from "react-icons/io";
 import { Link,  useLocation, Location } from "react-router-dom";
-import { RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
+import { RiCoupon3Fill, RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
 import { IconType } from "react-icons";
+import {FaChartBar,FaChartPie,FaChartLine, FaStopwatch, FaGamepad} from "react-icons/fa"
 
 const AdminSideBar = () => {
 
-    const Location = useLocation()
+    const location = useLocation()
   return (
     <aside>
       <h2>LOGo.</h2>
 
-      <div>
-        <h5>Dashboard</h5>
-        <ul>
+      <DivOne location={location} />
+      <DivTwo location={location} />
+      <DivThree location={location} />
 
-            <Li url="/admin/dashboard" text="Dashboard"  location={Location} Icon={RiDashboardFill}/>
-            <Li url="/admin/product" text="Product"  location={Location} Icon={RiShoppingBag3Fill}/>
-            <Li url="/admin/customer" text="Customer"  location={Location} Icon={IoIosPeople}/>
-            <Li url="/admin/transaction" text="Transaction"  location={Location} Icon={AiFillFileText}/>
-         
-          
-        </ul>
-      </div>
+    
+       
     </aside>
   );
 };
@@ -52,6 +47,91 @@ interface LiProps {
         {text}
       </Link>
     </li>
+  );
+
+  const DivOne = ({ location }: { location: Location }) => (
+    <div>
+      <h5>Dashboard</h5>
+      <ul>
+        <Li
+          url="/admin/dashboard"
+          text="Dashboard"
+          Icon={RiDashboardFill}
+          location={location}
+        />
+        <Li
+          url="/admin/product"
+          text="Product"
+          Icon={RiShoppingBag3Fill}
+          location={location}
+        />
+        <Li
+          url="/admin/customer"
+          text="Customer"
+          Icon={IoIosPeople}
+          location={location}
+        />
+        <Li
+          url="/admin/transaction"
+          text="Transaction"
+          Icon={AiFillFileText}
+          location={location}
+        />
+      </ul>
+    </div>
+  );
+
+  const DivTwo = ({ location }: { location: Location }) => (
+    <div>
+      <h5>Charts</h5>
+      <ul>
+        <Li
+          url="/admin/chart/bar"
+          text="Bar"
+          Icon={FaChartBar}
+          location={location}
+        />
+        <Li
+          url="/admin/chart/pie"
+          text="Pie"
+          Icon={FaChartPie}
+          location={location}
+        />
+        <Li
+          url="/admin/chart/line"
+          text="Line"
+          Icon={FaChartLine}
+          location={location}
+        />
+      </ul>
+    </div>
+  );
+
+
+  const DivThree = ({ location }: { location: Location }) => (
+    <div>
+      <h5>Apps</h5>
+      <ul>
+        <Li
+          url="/admin/app/stopwatch"
+          text="Stopwatch"
+          Icon={FaStopwatch}
+          location={location}
+        />
+        <Li
+          url="/admin/app/coupon"
+          text="Coupon"
+          Icon={RiCoupon3Fill}
+          location={location}
+        />
+        <Li
+          url="/admin/app/toss"
+          text="Toss"
+          Icon={FaGamepad}
+          location={location}
+        />
+      </ul>
+    </div>
   );
 
 export default AdminSideBar;
