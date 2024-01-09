224ebc144fc67dea18650f01926f3a02d70a441e
manish <manishkumar60708090@gmail.com>
2024-01-09 13:21:09 +0530

    dashboard heading section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,25 @@
 import AdminSideBar from "../components/AdminSideBar";
+import { BsSearch } from "react-icons/bs"
+import { FaRegBell } from "react-icons/fa"
+import userpic from "../assets/userpic.png"
+
 
 const Dashboard = () => {
   return (
     <div className="adminContainer">
       <AdminSideBar />
-      Dashboard
+      
+      <main className="dashboard">
+        <div className="bar">
+
+          <BsSearch  />
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell />
+          <img  src={userpic} alt="User"/>
+
+        </div>
+
+      </main>
     </div>
   );
 };
